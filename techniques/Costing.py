c81eb5f59d8973dec3f0bb74597070261a829d86
aristeidismoustakas <aristidis.moustakas@gmail.com>
2018-04-21 11:31:31 +0300

    Costing started.

---
+++ techniques/Costing.py
@@ -0,0 +1,11 @@
+from techniques.Technique import Technique
+
+class Costing(Technique):
+
+    def __init__(self):
+        super(Costing,self).__init__()
+
+    def run(self,model,dataset,cost_matrix):
+        pass
+
+
