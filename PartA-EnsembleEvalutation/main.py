f74f1f819d437debbfc6a1ac090da8753d636220
aristeidismoustakas <aris.moustakas@sigmia.com>
2018-04-22 13:12:14 +0300

    Average ranks added.

--- PartA-EnsembleEvalutation/main.py
+++ PartA-EnsembleEvalutation/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from datasets.BankCustomerDataset import BankCustomerDataset
 from datasets.CarEvaluationDataset import CarEvaluationDataset
 from datasets.ImageSegmentationDataset import ImageSegmentationDataset
@@ -38,7 +39,7 @@ models = {
     'Random Forest Classifier': RandomForestClassifier(),
     'AdaBoost Classifier binary': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200),
     #'AdaBoost Classifier multiclass': AdaBoostClassifier(DecisionTreeClassifier(max_depth=2),n_estimators=600, learning_rate=1.5, algorithm="SAMME"),
-    #'AdaBoost Classifier': AdaBoostClassifier(),
+    #'AdaBoost Classifier': AdaBoostClassifier(base_estimator=DecisionTreeClassifier(),n_estimators=100, random_state=1),
     'Gradient Boosting Classifier': GradientBoostingClassifier()
 }
 
@@ -47,7 +48,16 @@ scoring = {
     'f1_macro': 'f1_macro'
 }
 
+final_results = pd.DataFrame(index=datasets.keys(), columns=models.keys())
 results = {}
+
+
+def calculate_ranks(final_results):
+    ranks = final_results.rank(ascending=False, axis=1)
+    average_ranks = ranks.mean(axis=0)
+
+    return average_ranks
+
 
 # Train each model for each dataset on a N-Fold Cross Validation
 for dataset_name in datasets:
@@ -78,3 +88,7 @@ for dataset_name in datasets:
 
         for metric in results[dataset_name][model_name]:
             print('\t\t{}: {}'.format(metric, results[dataset_name][model_name][metric]))
+        final_results.loc[dataset_name, model_name] = results[dataset_name][model_name]['accuracy']
+
+average_ranks = calculate_ranks(final_results)
+print('\nThe average ranks of the algorithms are:\n {}'.format(average_ranks))
